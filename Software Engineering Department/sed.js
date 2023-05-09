0d85d89aee815a12f8d3747cd584d49cf2b3ecd4
Kashifjavedkainth <116870763+Kashifjavedkainth@users.noreply.github.com>
2023-05-09 22:48:32 +0500

    Add files via upload

---
+++ Software Engineering Department/sed.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>My Website</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <h1>Welcome to My Website</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui ac augue fringilla malesuada sed et orci. Integer commodo lacus purus, sit amet tristique magna viverra vel. Mauris id libero ut lorem tincidunt aliquam. Fusce non enim ac mi tincidunt scelerisque. Nam ac lorem a dolor lacinia lobortis non eget nisi. Pellentesque mollis malesuada eros at congue.</p>
+    </main>
+
+    <footer>
+      <p>&copy; 2023 My Website</p>
+    </footer>
+
+    <script src="script.js"></script>
+  </body>
+</html>
